d29d308bbdab105ebf62bfca1f1be47e09d32a3a
cjaytechguy <cjaytechguy@gmail.com>
2021-03-11 11:13:33 +0100

    fixed issue with error message

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  // apiBaseUrl: process.env.REACT_APP_API_URL,
-  apiBaseUrl: 'https://api.uat.buysafehouse.com/api',
+  apiBaseUrl: process.env.REACT_APP_API_URL,
+  // apiBaseUrl: 'https://api.uat.buysafehouse.com/api',
 }
 
 export default config
